acdade2499fa662c76831976032ad24eea255590
Night Coder <himanshunagose1806@gmail.com>
2024-02-14 12:03:13 +0530

    New Added

--- Library Management System/src/library_management_system/DisplayStudent.java
+++ Library Management System/src/library_management_system/DisplayStudent.java
@@ -1,17 +1,23 @@
 package library_management_system;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import net.proteanit.sql.DbUtils;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 
 public class DisplayStudent extends JFrame implements ActionListener {
 
+    // Add a new class variable to hold the image
+    private Image backgroundImage;
+
     Choice crollno;
     JTable table;
     JButton login, cancel;
-    JTextField tfusername,tfpassword;
+    JTextField tfpasskey,tfpassword;
 
     DisplayStudent() {
         super("DISPLAY STUDENT");
@@ -20,15 +26,53 @@ public class DisplayStudent extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
 
-        JLabel lblusername = new JLabel("Passkey");
-        lblusername.setBounds(150, 40, 150, 30);
-        lblusername.setFont(new Font("serif",Font.BOLD, 23));
-        add(lblusername);
-        
-        tfusername = new JTextField();
-        tfusername.setBounds(300, 40, 200, 30);
-        add(tfusername);
+        try {
+            backgroundImage = ImageIO.read(new File("path/to/your/image.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        // Create a new JPanel to hold your components
+        JPanel content = new JPanel() {
+ {           // Set the panel's layout to null
+            setLayout(null);
+            // Add the image to the panel
+            JLabel background = new JLabel(new ImageIcon(backgroundImage));
+            background.setBounds(0, 0, getWidth(), getHeight());
+            add(background);
+
+            // Add your components to the panel as before
+            JLabel lblpasskey = new JLabel("Passkey");
+            lblpasskey.setBounds(70, 40, 150, 30);
+            lblpasskey.setFont(new Font("serif",Font.BOLD, 23));
+            background.add(lblpasskey);
+            
+            
+            tfpasskey = new JTextField();
+            tfpasskey.setBounds(220, 40, 200, 30);
+            add(tfpasskey);
+
+            JLabel lblpassword = new JLabel("Password");
+            lblpassword.setBounds(520, 40, 150, 30);
+            lblpassword.setFont(new Font("serif", Font.BOLD, 23));
+            add(lblpassword);
+
+            tfpassword = new JTextField();
+            tfpassword.setBounds(670, 40, 200, 30);
+            add(tfpassword);
+
+            
+            table = new JTable();
+            JScrollPane jsp = new JScrollPane(table);
+            jsp.setBounds(15, 150, 950, 200);
+            add(jsp);
+            
+            // Add the panel to the frame
+            add(content);
+            setVisible(true);
+    }
+}
+    public void showFrame() {
         login = new JButton("Login");
         login.setBounds(180, 90, 100, 30);
         login.addActionListener(this);
@@ -38,18 +82,12 @@ public class DisplayStudent extends JFrame implements ActionListener {
         cancel.setBounds(320, 90, 150, 30);
         cancel.addActionListener(this);
         add(cancel);
-
-        table = new JTable();
-        JScrollPane jsp = new JScrollPane(table);
-        jsp.setBounds(15, 150, 950, 200);
-        add(jsp);
-
-        setVisible(true);
     }
 
+    @Override
     public void actionPerformed (ActionEvent ae) {
         if (ae.getSource() == login) {
-            String query = "select * from ADDUSER where passkey = '"+tfusername.getText()+"'";
+            String query = "select * from ADDUSER where passkey = '"+tfpasskey.getText()+"' and password = '"+tfpassword.getText()+"'";
             try {
                 Conn c = new Conn();
                 ResultSet rs = c.s.executeQuery(query);
